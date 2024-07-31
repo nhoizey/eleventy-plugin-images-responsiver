83aa7714b110171fa0af6585e38140d989c0b44a
Nicolas Hoizey <nicolas@hoizey.com>
2024-07-31 19:12:12 +0200

    feat: check Eleventy version compatibility

--- eleventy-plugin-images-responsiver.js
+++ eleventy-plugin-images-responsiver.js
@@ -1,8 +1,20 @@
 'use strict';
 
+import pkg from 'package.json' with { type: 'json' };
+
 const imagesResponsiver = require('images-responsiver');
 
-module.exports = (eleventyConfig, options = {}) => {
+export default (eleventyConfig, options = {}) => {
+	// First check if the plugin is used with a compatible version of Eleventy
+	try {
+		eleventyConfig.versionCheck(pkg['11ty'].compatibility);
+	} catch (e) {
+		console.error(
+			`[eleventy-plugin-image-responsiver] Plugin compatibility error ${e.message}`
+		);
+		return;
+	}
+
 	eleventyConfig.addTransform('imagesResponsiver', function (content) {
 		if (this.page.outputPath && this.page.outputPath.endsWith('.html')) {
 			return imagesResponsiver(content, options, this.page.url);
